91beddba8fea5520707e6997777371484013b535
1rma0lh0 <YOUR_EMAIL>
2024-03-31 14:48:06 -0300

    Web3+Solc

---
+++ Legi-o.solc/contracts/Legiao.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12 <0.9.0;
+
+
+contract Legiao {
+  /**
+   * @dev Prints Legião string
+   */
+  function print() public pure returns (string memory) {
+    return "Legiao!";
+  }
+}
