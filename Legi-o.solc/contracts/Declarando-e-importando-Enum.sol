4a0ed5bb6e90aa22d330c82940727ecd1dddfdb2
Nick <160781135+Nicolau-369@users.noreply.github.com>
2024-10-19 17:44:06 -0300

    Update Declarando-e-importando-Enum.sol

--- Legi-o.solc/contracts/Declarando-e-importando-Enum.sol
+++ Legi-o.solc/contracts/Declarando-e-importando-Enum.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.12 <0.9.0;
-// Nosso enum foi salvo com nome de 'EnumStatus.sol'
+// Nosso enum foi salvo com nome de 'Declarando-e-importando-Enum.sol'
 
 enum Status {
     Pending,
