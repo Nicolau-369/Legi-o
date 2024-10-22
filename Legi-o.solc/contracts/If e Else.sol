fcca9edc91caffe759cb688ef837764e083f20a3
Nick <160781135+Nicolau-369@users.noreply.github.com>
2024-10-22 17:19:37 -0300

    Update If e Else.sol

--- Legi-o.solc/contracts/If e Else.sol
+++ Legi-o.solc/contracts/If e Else.sol
@@ -1,30 +1,31 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity >=0.6.12 <0.9.0;
 
 contract IfElse {
-    function foo(uint x) public pure returns (uint) {
+    function fooo(uint x) public pure returns (uint) {
         if (x < 10) {
             return 0;
         } else if (x < 20) {
             return 1;
         } else {
-            return 2;
+            return 2
         }
     }
 
-    // Utilizando o if ternário, podemos reduzir as linhas de código,
-    // facilitando nossa leitura do código
+    // Using the ternary if, we can reduce the lines of code,
+    // making it easier to read the code
     function ternario(uint _x) public pure returns (uint) {
         // if (_x < 10) {
         //     return 1;
         // }
         // return 2;
 
-        // Maneira abreviada de escrever a instrução if / else
-        // Nessa instrução, se o _x for menor que 10, 
-        // retornará true e então nos retornar o valor 
-        // que está após o ?, ou seja o 1, em caso de false, 
-        // irá retornar o valor após o :, ou seja o 2
+        // Shorthand way of writing if/else statement
+        // In this instruction, if _x is less than 10, 
+        // will return true and then return the value 
+        // which is after the ?, that is, the 1, in case of false, 
+        // will return the value after the :, i.e. 2
         return _x < 10 ? 1 : 2;
     }
 }
