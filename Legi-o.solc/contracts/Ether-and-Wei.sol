5976f555deb09c672109de2fec456c8660c8323c
Nick <160781135+Nicolau-369@users.noreply.github.com>
2024-10-22 17:05:37 -0300

    Update and rename Ether-e-Wei.sol to Ether-and-Wei.sol

--- Legi-o.solc/contracts/Ether-and-Wei.sol
+++ Legi-o.solc/contracts/Ether-and-Wei.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity >=0.6.12 <0.9.0;
 
-contract EtherUnits {
+contract EtherUnits{
     uint public umWei = 1 wei;
-    // 1 wei é igual a 1
+    // 1 wei is equal to 1
     bool public ehUmWei = 1 wei == 1;
 
     uint public umEther = 1 ether;
-    // 1 ether é igual a 10^18 wei
+    // 1 ether is equal to 10^18 wei
     bool public ehUmEther = 1 ether == 1e18;
 }
