90a36f08cd094ecd894784663640b58bbae6ba1a
Nick <160781135+Nicolau-369@users.noreply.github.com>
2024-10-22 00:26:35 -0300

    Update Array(matrizes).sol

--- Legi-o.solc/contracts/Array(matrizes).sol
+++ Legi-o.solc/contracts/Array(matrizes).sol
@@ -1,53 +1,54 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity >=0.6.12 <0.9.0;
 
 contract Array {
-    // Maneiras para definir uma array
-    uint[] public arr; // Array de tamanho dinâmico
-    // Array de tamanho dinâmico inicializada com os valores [1, 2, 3]
+    // Ways to define an array
+    uint[] public arr; // Dynamic Size Array
+    // Dynamically sized array initialized with values ​​[1, 2, 3]
     uint[] public arr2 = [1, 2, 3];
 
-    // Array de tamanho fixo, todos os elementos inicializam em 0
-    uint[10] public myFixedSizeArr;
+    // Fixed size array, all elements initialize to 0
+    uint[10] public myFixedSizedArr;
 
     function get(uint i) public view returns (uint) {
         return arr[i];
     }
 
-    // Solidity pode retornar o array inteiro
-    // Mas esta função deve ser evitada para arrays 
-    // que podem crescer indefinidamente em tamanho
+    // Solidity can return the entire array
+    // But this function should be avoided for arrays 
+    // which can grow indefinitely in size
     function getArr() public view returns (uint[] memory) {
         return arr;
     }
 
     function push(uint i) public {
-        // Adiciona um valor na matriz
-        // Isso aumentará o tamanho da matriz em 1
+        // Add a value to the array
+        // This will increase the size of the array by 1
         arr.push(i);
     }
 
     function pop() public {
-        // Remove o último valor adicionado
-        // Isso diminuirá o tamanho da matriz em 1
+        // Remove the last added value
+        // This will decrease the size of the array by 1
         arr.pop();
     }
 
     function getLength() public view returns (uint) {
-        // Retorna o tamanho da matriz
+        // Returns the size of the matrix
         return arr.length;
     }
 
     function remove(uint index) public {
-        // Excluir o valor de um índice não irá diminuir o valor da matriz.
-        // Irá definir o valor no índice para o valor padrão do tipo,
-        // neste caso, o valor será setado para 0
+        // Deleting the value of an index will not decrease the value of the array.
+        // Will set the value at index to the default value of the type,
+        // in this case, the value will be set to 0
         delete arr[index];
     }
 
     function examples() external {
-        // Cria uma nova matriz na memória
-        // Somente matrizes com tamanho fixo podem ser criadas assim;
-        uint[] memory a = new uint[](5)
+        // Create a new array in memory
+        // Only fixed size matrices can be created like this
+        uint[] memory a = new uint[] (5)
     }
 }
