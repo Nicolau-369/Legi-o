e1a4ec89410fc3cab7c37fc6fb33b582cbebdad1
Nick <160781135+Nicolau-369@users.noreply.github.com>
2024-10-31 22:34:57 -0300

    Update and rename mapping(aplicacao).sol to mapping(application).sol

--- Legi-o.solc/contracts/mapping(application).sol
+++ Legi-o.solc/contracts/mapping(application).sol
@@ -1,18 +1,19 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity >=0.6.12 <0.9.0;
 
 contract GameScore {
-    // Definimos nosso mapa como playerScores que irá 
-    // receber o endereço do jogador e a pontuação
+    // We define our map as playerScores which will 
+    // receive the player's address and score
     mapping(address => uint) public playerScores;
 
     function getScore(address _addr) public view returns (uint) {
-        // Iremos retornar o score atual do jogador
+        // We will return the player's current score
         return playerScores[_addr];
     }
 
     function setScore(address _addr, uint _score) public {
-        // Atualiza o score do jogador
+        // Update the player's score
         playerScores[_addr] = _score;
     }
 }
